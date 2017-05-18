98599a4d1710381ce937c2ff3d0f3b9768eb0de5
刘翔宇 <ITliuxiangyu@163.com>
2017-05-18 17:01:39 +0800

    Update new_file.php

--- new_file.php
+++ new_file.php
@@ -2,6 +2,8 @@
 	header("Access-Control-Allow-Origin: *");
   $myUrl = $_GET["myUrl"];
   
+  // 我该了代码
+  
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $myUrl);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
